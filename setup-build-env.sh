098b8a19b51a774ce251ecc96d710ce521c4f85c
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2022-11-05 17:08:13 +0000

    moved things around so we can merge them easier later

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -10,15 +10,14 @@ tar xjf arduino-cli.tar.bz2
 rm arduino-cli.tar.bz2
 mv `ls -1` /usr/bin/arduino-cli
 
-# Install python, pip and pyserial
+# Install python, pip and python packages
 apt-get install python3.7 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-#pip install pyserial
+#pip install numpy
 
 # install libraries and boards
 arduino-cli core update-index
 arduino-cli core install arduino:avr
-arduino-cli lib install arduino:megaavr
 arduino-cli lib install "Adafruit Motor Shield V2 Library"
 arduino-cli lib install "WiFiNINA"
