652b6dee045dcfc08b89eed9fb63e99a11fecea4
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2022-11-05 11:36:41 +0000

    inlcuding fix from https://github.com/arduino/arduino-cli/issues/6

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -10,6 +10,11 @@ tar xjf arduino-cli.tar.bz2
 rm arduino-cli.tar.bz2
 mv `ls -1` /usr/bin/arduino-cli
 
+sudo mkdir -p .arduino15/
+cd .arduino15/
+wget https://downloads.arduino.cc/packages/package_index.json
+cd ~
+
 # Install python, pip and pyserial
 apt-get install python -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
