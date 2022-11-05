775ce2e90b6f7f13dfaa36ae98cb5f1c0268231a
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2022-11-05 11:19:37 +0000

    CI/CD setup to test build releases

---
+++ setup-build-env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+apt-get update
+cd ~
+ 
+# Install arduino-cli
+apt-get install curl -y
+curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
+tar xjf arduino-cli.tar.bz2
+rm arduino-cli.tar.bz2
+mv `ls -1` /usr/bin/arduino-cli
+
+# Install python, pip and pyserial
+apt-get install python -y
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+#pip install pyserial
+
+# install libraries
+arduino-cli lib install "Adafruit BME280 Library"
+arduino cli lib install "WiFiNINA"
