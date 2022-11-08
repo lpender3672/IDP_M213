5944110cba79cd8ec9da7ecd62b17163b0cc8b41
infinus <yeheng2001@gmail.com>
2022-11-08 12:59:13 +0000

    basic svelte

--- go/main.go
+++ go/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
+
+	"github.com/hybridgroup/mjpeg"
 	"gocv.io/x/gocv"
 )
 
+
+var (
+	// deviceID int
+	err      error
+	webcam   *gocv.VideoCapture
+	stream   *mjpeg.Stream
+)
+
 func main() {
+
+	// open webcam
+	webcam, err := gocv.OpenVideoCapture("http://localhost:8081/stream/video.mjpeg")
+	if err != nil {
+		fmt.Printf("Error opening capture device")
+		return
+	}
+	defer webcam.Close()
+
+	// create the mjpeg stream
+	stream = mjpeg.NewStream()
+
+	// start capturing
+	go mjpegCapture()
 
 	engine := html.New("./templates", ".html")
 
@@ -15,19 +41,36 @@ func main() {
     })
     app.Static("/public", "./public")
     app.Get("/", mainPage)
+	app.Get("/stream", stream)
     app.Listen(":3000")
 
-	webcam, _ := gocv.OpenVideoCapture("http://localhost:8081/stream/video.mjpeg")
-	window := gocv.NewWindow("Hello")
-	img := gocv.NewMat()
-
-	for {
-		webcam.Read(&img)
-		window.IMShow(img)
-		window.WaitKey(1)
-	}
+	
 }
 
 func mainPage(c *fiber.Ctx) error {
     return c.Render("mainpage", nil)
+}
+
+// // This route path will match requests to the root route, "/":
+// func streamer(c *fiber.Ctx) error {
+// 	return c.SendStream(stream)
+// }
+
+func mjpegCapture() {
+	img := gocv.NewMat()
+	defer img.Close()
+
+	for {
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed")
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		buf, _ := gocv.IMEncode(".jpg", img)
+		stream.UpdateJPEG(buf.GetBytes())
+		buf.Close()
+	}
 }
