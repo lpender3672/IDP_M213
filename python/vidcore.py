1b673edf355f1474164a552638e523ea4e065237
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-11-23 09:30:38 +0000

    block location done

--- python/vidcore.py
+++ python/vidcore.py
@@ -4,100 +4,23 @@ import cv2 as cv
 import math
 from icecream import ic
 
-
-
-
-# def getUndistort(cap: VideoCapture, balance=0.0, dim2=None, dim3=None):
-#     img = cap.read()
-#     dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort    
-#     if not dim2:
-#         dim2 = dim1    
-#     if not dim3:
-#         dim3 = dim1    
-#         scaled_K = K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
-#     scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0    # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. OpenCV document failed to make this clear!
-#     new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
-#     map1, map2 = cv.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv.CV_16SC2)
-#     undistorted_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT) 
-
-#     return undistorted_img
-
-
-# def getRotM(cap: VideoCapture, nframes: int = 20):
-
-#     (height, width ,dim) = np.shape(getUndistort(cap))
-#     # ic(width)
-    
-#     rotTheta = np.zeros(nframes)
-#     for r in range(nframes):
-#         dist = getUndistort(cap)
-#         hsv = cv.cvtColor(dist, cv.COLOR_BGR2HSV) #src BGR to HSV	
-#     frame_threshold = cv.inRange(hsv, (0, 0, 228), (255, 255, 255))
-#     frame_threshold = cv.GaussianBlur(frame_threshold, (9, 9), 1);
-#     kernel = np.ones((19, 19), np.uint8)
-#     dilation = cv.dilate(frame_threshold, kernel, iterations=1) 
-#     erosion = cv.erode(dilation, kernel, iterations=1)
-#     erosionG = cv.Canny(erosion, 40, 200, None, 3)
-#     distO = np.copy(dist)    
-
-#     rotTheta[r] = 0
-
-#     lines = cv.HoughLines(erosionG, 1, np.pi / 250, 250, None, 0, 0)
-#     # ic(lines)
-#     if lines is not None:
-#         for i in range(0, len(lines)):
-#             rho = lines[i][0][0]
-#             theta = lines[i][0][1]
-#             a = math.cos(theta)
-#             b = math.sin(theta)
-#             x0 = a * rho
-#             y0 = b * rho
-#             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-#             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-#             cv.line(distO, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
-#             # cv.imshow("line", distO)
-#             try:
-#                 rotTheta[r] += (pt1[1]-pt2[1])/(pt1[0]-pt2[0])
-#                 # ic(theta)
-#             except ZeroDivisionError:
-#                 ic("Zero in Line Detection")
-
-#         rotTheta[r] = rotTheta[r] / len(lines)
-    
-#     rotThetaAvg = np.average(rotTheta)
-#     rotThetaAvg = math.atan(rotThetaAvg)
-#     rotThetaAvg += np.pi/2
-#     rotM = cv.getRotationMatrix2D((width/2, height/2), rotThetaAvg * 180 / np.pi, 1) #???????
-
-#     # return rotThetaAvg
-#     return rotM
-
-
-# def getCorrectedFrame(cap: VideoCapture, rotM, resizeFactor = 1, blurSize = 1):
-
-#     (_height, _width,_dim) = np.shape(getUndistort(cap))
-#     dist = getUndistort(cap)
-    
-
-#     distOR = cv.warpAffine(src=dist, M=rotM, dsize=(_width, _height))
-#     dim = (int(_width/resizeFactor), int(_height/resizeFactor))
-#     distOR = cv.GaussianBlur(distOR, (blurSize, blurSize), 1)
-#     distOR = cv.resize(distOR, dim, interpolation = cv.INTER_AREA)
-
-#     return distOR
-
 class vid:
     DIM=(1012, 760)
     K = np.array([[567.4130565572482, 0.0, 501.39791714355], [0.0, 567.3325405728447, 412.9039077874256], [0.0, 0.0, 1.0]])
     D = np.array([[-0.05470334257497442], [-0.09142371384400942], [0.17966906821072895], [-0.08708720575337928]])
     
     def __init__(self, name) -> None:
-        # self.cap = VideoCapture(name)
-        self.cap = cv.VideoCapture(name)
+        self.cap = VideoCapture(name)
+        # self.cap = cv.VideoCapture(name)
         pass
 
+    def getRaw(self):
+        img = self.cap.read()
+        return img
+
     def getUndistort(self, balance=0.0, dim2=None, dim3=None):
-        ret, img = self.cap.read()
+        img = self.cap.read()
+        # ret, img = self.cap.read()
         dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort    
         if not dim2:
             dim2 = dim1    
@@ -111,7 +34,7 @@ class vid:
 
         return undistorted_img
 
-    def getRotM(self, nframes: int = 20):
+    def getRotMHL(self, nframes: int = 20):
 
         (height, width ,dim) = np.shape(self.getUndistort(self.cap))
         # ic(width)
@@ -130,7 +53,7 @@ class vid:
 
         rotTheta[r] = 0
 
-        lines = cv.HoughLines(erosionG, 1, np.pi / 250, 250, None, 0, 0)
+        lines = cv.HoughLines(erosionG, 1, np.pi / 250, 220, None, 0, 0)
         # ic(lines)
         if lines is not None:
             for i in range(0, len(lines)):
@@ -155,10 +78,44 @@ class vid:
         rotThetaAvg = np.average(rotTheta)
         rotThetaAvg = math.atan(rotThetaAvg)
         rotThetaAvg += np.pi/2
+        # ic(rotThetaAvg)
         self.rotM = cv.getRotationMatrix2D((width/2, height/2), rotThetaAvg * 180 / np.pi, 1) #???????
 
         # return rotThetaAvg
         return self.rotM
+
+    #WARN: Broken with blocks present
+    def getRotMMAR(self): #min area rect implementation
+        (height, width ,dim) = np.shape(self.getUndistort(self.cap))
+        pathLo = (0, 0, 180)
+        pathHi = (255, 30, 255)
+        img = self.getUndistort(self.cap)
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+        tresh_path = cv.inRange(hsv,pathLo, pathHi)
+        # rkernel = np.ones(self.pathKernel, np.uint8)    
+        
+        tresh_path = cv.erode(tresh_path, (5,5), iterations=3)
+        tresh_path = cv.dilate(tresh_path, (3,3), iterations=3) 
+
+        # cv.imshow('thres', tresh_path)
+
+        cnt, _ = cv.findContours(tresh_path, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cnt = sorted(cnt, key = lambda x: cv.arcLength(curve = x, closed=False), reverse=True) #get largest contour
+        cnt = cnt[0]
+        rect = cv.minAreaRect(cnt)
+        # box = cv.boxPoints(rect)
+        # box = np.int0(box)
+        # cv.drawContours(img,[box],0,(0,0,255),2)
+        # cv.drawContours(cnt,[box],0,(0,0,255),2)
+        # cv.imshow("box", img)
+        # cv.waitKey(1000000)
+        # ic(rect)
+        self.rotM = cv.getRotationMatrix2D((width/2, height/2), rect[2], 1)
+        return self.rotM
+
+
+
 
     def getCorrectedFrame(self, resizeFactor = 1, blurSize = 1):
 
@@ -178,10 +135,10 @@ class vid:
 
 if __name__ == "__main__":
 
-    # cap = vid("http://localhost:8081/stream/video.mjpeg")
     
+    # cap = vid("http://localhost:8081/stream/video.mjpeg")
     cap = vid("C:\\Users\\yehen\\Videos\\2022-11-10 09-09-04.m4v")
-    cap.getRotM() #enable rotation
+    cap.getRotMMAR() #enable rotation
 
     while True:
         img = cap.getCorrectedFrame(2)
--- python/videoproc.py
+++ python/videoproc.py
@@ -9,33 +9,43 @@ class arena:
 
     #config vars
     downscale = 2
+    bakRotThetaAvg = 1.5707963267948966
 
     pathLo = (0, 0, 180)
-    pathHi = (255, 16, 255)
+    pathHi = (255, 30, 255)
     pathKernel = (3,3)
 
     gLo = (66, 30, 70)
     gHi = (85, 255, 255)
     gKernel = (3,3)
 
-    rLo = (80, 33, 41)
-    rHi = (125, 255, 243)
+    rLo = (100, 33, 41)
+    rHi = (120, 255, 243)
     rKernel = (3,3)
 
-    oLo = (27, 91, 182)
+    oLo = (27, 50, 182)
     oHi = (52, 255, 255)
     oKernel = (15,5)
+
+    robotMedianBlur = 5
+    robotLo = (30, 50, 0)
+    robotHi = (50, 255, 255)
+
+    blockLo = (0, 0, 0)
+    blockHi = (210, 255, 110)
+    blockSmall = 70 #blockSizes
+    blockLarge = 120
 
     ntmax = 15    
 
 
     def __init__(self, name):
         self.cap = vid(name)
-        ic(self.cap.getRotM()) #initialise rotation too
+        ic(self.cap.getRotMHL()) #initialise rotation too
         
 
     def drawDiagnosticPoint(self, frame, point, color):
-        return cv.circle(frame, point, radius=1, color=color, thickness=-1)
+        return cv.circle(frame, point, radius=3, color=color, thickness=-1)
 
     def grabAll(self, nframes = 20):
 
@@ -97,19 +107,22 @@ class arena:
         self.path = np.median(self.path, axis=-1).astype(np.uint8)
         self.obox = np.median(self.obox, axis=-1).astype(np.uint8)
 
+        cv.imshow("obox", self.rbox)
+        # cv.waitKey(500)
+
         #get path mask
-        self.pathMask = np.zeros(np.shape(self.path))
-        pathCnts,_ = cv.findContours(self.path, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-        maxPathMoment = 0
-        maxPath = 0
-        for i in range(len(pathCnts)): #find largest contour by extents
-            currMoment = cv.moments(pathCnts[i])["m00"]
-            if currMoment > maxPathMoment:
-                maxPathMoment = currMoment
-                maxPath = i
-        self.pathMask = cv.drawContours(self.pathMask, pathCnts, maxPath, color=(255,255,255))
-        self.pathMask = cv.dilate(self.pathMask, None, iterations=2)
-        self.path = cv.bitwise_and(self.path, self.pathMask.astype(np.uint8)) #mask path
+        # self.pathMask = np.zeros(np.shape(self.path))
+        # pathCnts,_ = cv.findContours(self.path, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        # maxPathMoment = 0
+        # maxPath = 0
+        # for i in range(len(pathCnts)): #find largest contour by extents
+        #     currMoment = cv.moments(pathCnts[i])["m00"]
+        #     if currMoment > maxPathMoment:
+        #         maxPathMoment = currMoment
+        #         maxPath = i
+        # self.pathMask = cv.drawContours(self.pathMask, pathCnts, maxPath, color=(255,255,255))
+        # self.pathMask = cv.dilate(self.pathMask, None, iterations=2)
+        # self.path = cv.bitwise_and(self.path, self.pathMask.astype(np.uint8)) #mask path
 
         # find green square
         cnts,_ = cv.findContours(self.gbox, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -152,9 +165,70 @@ class arena:
 
         self.t0XY = (whiteCols[0][0], whiteRows[0][0])
         self.t1XY = (whiteCols[0][0], whiteRows[0][-1])
+
+        #extrapolate to find ramp start and end
+        rampRow = self.path[whiteRows[0][0]]
+        whiteCols = np.where(rampRow == 255)
+        self.r0XY = (whiteCols[0][0], whiteRows[0][0])
+        rampRow = self.path[whiteRows[0][-1]]
+        whiteCols = np.where(rampRow == 255)
+        self.r1XY = (whiteCols[0][0], whiteRows[0][-1])
+
+    def findRobot(self):
+        img = self.cap.getCorrectedFrame(self.downscale)
+        # shape = np.shape(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
         
-        ic(self.t0XY)
-        ic(self.t1XY)       
+        mb = cv.medianBlur(img, self.robotMedianBlur)
+        mb = cv.cvtColor(mb, cv.COLOR_BGR2HSV)
+        robot_tresh = cv.inRange(mb, self.robotLo, self.robotHi)
+        # robot_tresh = cv.GaussianBlur(robot_tresh, (self.robotMedianBlur, self.robotMedianBlur), 1)
+        # cv.imshow("rt", robot_tresh)
+        rows = robot_tresh.shape[0]
+
+        circles = cv.HoughCircles(robot_tresh, cv.HOUGH_GRADIENT, 1, rows / 8, param1=10, param2=12, minRadius=5, maxRadius=10)
+        # ic(circles)
+        # if circles is not None:
+        #     circles = np.uint16(np.around(circles))
+        #     for i in circles[0, :]:
+        #         center = (i[0], i[1])
+        #         # circle center
+        #         cv.circle(img, center, 1, (0, 100, 100), 3)
+        #         # circle outline
+        #         radius = i[2]
+        #         cv.circle(img, center, radius, (255, 0, 255), 3)
+        # cv.imshow("mb",img)
+        if circles is not None:
+            return (circles[0][:1]) #return circle center coordinates
+
+    def detectBlocks(self):
+        img = self.cap.getCorrectedFrame(self.downscale)
+
+        luv = cv.cvtColor(img, cv.COLOR_BGR2LUV)
+        # l,u,v = cv.split(luv)
+
+        thres = cv.inRange(luv,(0, 0, 0),(210, 255, 110))
+        # thresint = cv.cvtColor(thres, cv.COLOR_GRAY2BGR)
+        # cv.imshow("ti", thresint)
+        # cv.imshow('l', l)
+        # cv.imshow('u', u)
+        # cv.imshow('v', v)
+
+        cnts, _ = cv.findContours(thres, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        blocks = []
+        for c in cnts:
+            M = cv.moments(c)
+            # ic(M['m00'])
+            if M["m00"] < self.blockLarge and M["m00"] > self.blockSmall:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                blocks.append((cx, cy))
+        
+        return blocks
+
+    def identifyNearestBlock(self):
+        pass
+        
+
 
     def getDiagnosticMap(self):
         self.grabAll()
@@ -163,16 +237,26 @@ class arena:
         self.map = self.drawDiagnosticPoint(self.map, self.gXY, (0,255,0))
         self.map = self.drawDiagnosticPoint(self.map, self.t0XY, (0,255,255))   
         self.map = self.drawDiagnosticPoint(self.map, self.t1XY, (0,255,255))
+        self.map = self.drawDiagnosticPoint(self.map, self.r1XY, (255,0,255))   
+        self.map = self.drawDiagnosticPoint(self.map, self.r0XY, (255,0,255))
+        blocks = self.detectBlocks()
+        if(len(blocks) > 0):
+            for b in blocks:
+                self.map = self.drawDiagnosticPoint(self.map, b, (255,0,0))
         return self.map
 
         
 
 
 if __name__ == "__main__":
-    # map = arena("http://localhost:8081/stream/video.mjpeg")
-    map = arena("C:\\Users\\yehen\\Videos\\2022-11-10 09-09-04.m4v")
+    map = arena("http://localhost:8081/stream/video.mjpeg")
+    img = map.getDiagnosticMap()
+    # map = arena("C:\\Users\\yehen\\Videos\\2022-11-10 09-09-04.m4v")
     while True:
-        img = map.getDiagnosticMap()
+        
+        # map.findRobot()
+        map.detectBlocks()
+        # img = map.getDiagnosticMap()
 
         cv.imshow("test", img)
         if cv.waitKey(1) == ord('q'):
